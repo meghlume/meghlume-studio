e85aa9651cc3b6af79f2fe1a478dc56bd8d3ee49
meghlume <meghlume7433@gmail.com>
2026-07-29 17:34:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+
+    if (menu.style.right === "0px") {
+        menu.style.right = "-260px";
+    } else {
+        menu.style.right = "0px";
+    }
+}
