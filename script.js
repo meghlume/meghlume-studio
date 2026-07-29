840753128e93d8148d066ece4931bf7393dbc1ce
meghlume <meghlume7433@gmail.com>
2026-07-29 22:27:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("mobileMenu");
-
-    if (menu.style.right === "0px") {
-        menu.style.right = "-260px";
-    } else {
-        menu.style.right = "0px";
-    }
-}
